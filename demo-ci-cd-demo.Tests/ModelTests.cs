9afead40b2a06148ec40ff3f20fdf7eb4fba93d8
Ian Simpson <ian.simpson@housing21.org.uk>
2025-12-04 09:41:04 +0000

    Added unit tests

---
+++ demo-ci-cd-demo.Tests/ModelTests.cs
@@ -0,0 +1,21 @@
+﻿namespace demo_ci_cd_demo.Tests
+{
+    public class ModelTests
+    {
+        [Fact]
+        public void WeatherForecast_Test()
+		{
+			var date = DateOnly.FromDateTime(DateTime.Now);
+            int temperatureC = 25;
+            string summary = "Sunny";
+
+            WeatherForecast weatherForecast = new(date, temperatureC, summary);
+            
+            Assert.Equal(date, weatherForecast.Date);
+            Assert.Equal(temperatureC, weatherForecast.TemperatureC);
+            Assert.Equal(summary, weatherForecast.Summary);
+
+			Assert.Equal(32 + (int)(temperatureC / 0.5556), weatherForecast.TemperatureF);
+		}
+    }
+}
